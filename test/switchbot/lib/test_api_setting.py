097a3217c413b451bf0470c46a145478ee89040d
OKAZAKI SHogo <okazaki@zakioka.net>
2024-10-08 01:05:37 +0900

    pytest  を導入し、最初のテストを書く

---
+++ test/switchbot/lib/test_api_setting.py
@@ -0,0 +1,15 @@
+import time
+from app.switchbot.lib.api_setting import create_api_header
+import uuid
+import pytest
+
+
+def test_create_api_header_正しくヘッダーが設定されること():
+    """'Authorization' 以外はランダムに決まるか固定値が返されるだけなので、
+    ここでは、渡した token が 'Authorization' の値として返ってくることのみテストする
+     """
+    token = "test_token"
+    secret = "dummy_secret"
+    result = create_api_header(token=token, secret=secret)
+    expect = token
+    assert result["Authorization"] == expect
