bebbda4dca910f1eaaf873b28512ffe547b03517
OKAZAKI SHogo <okazaki@zakioka.net>
2024-10-08 23:29:08 +0900

    ヘッダーのキーを比較するテストを追加

--- test/lib/switchbot/test_api_setting.py
+++ test/lib/switchbot/test_api_setting.py
@@ -4,7 +4,7 @@ import uuid
 import pytest
 
 
-def test_create_api_header_正しくヘッダーが設定されること():
+def test_create_api_header_Authorizationヘッダーが正しく設定されること():
     """'Authorization' 以外はランダムに決まるか固定値が返されるだけなので、
     ここでは、渡した token が 'Authorization' の値として返ってくることのみテストする
      """
@@ -13,3 +13,10 @@ def test_create_api_header_正しくヘッダーが設定されること():
     result = create_api_header(token=token, secret=secret)
     expect = token
     assert result["Authorization"] == expect
+
+def test_create_api_header_ヘッダーのキーが正しく設定されること():
+    token = "test_token"
+    secret = "dummy_secret"
+    result = list(create_api_header(token=token, secret=secret).keys())
+    expect = ['Authorization', 'Content-Type', 'charset', 't', 'sign', 'nonce']
+    assert set(result) == set(expect)
