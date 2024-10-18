04cf13782dd8d088f0d927becbff01815f1301d2
OKAZAKI Shogo <okazaki@zakioka.net>
2024-10-18 23:26:28 +0900

    スプレッドシートに温湿度計の取得情報を書き込む処理を追加

--- app/receive_data_regularly.py
+++ app/receive_data_regularly.py
@@ -3,9 +3,13 @@ from dotenv import load_dotenv
 import os
 from os.path import join, dirname
 import json
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 import requests
 
+from lib.switchbot.api_setting import create_api_header
+from lib.google_spread_sheet.google_spread_sheet import getGoogleSpreadSheet
 from lib.switchbot.api_setting import create_api_header
 
 # 環境変数設定
@@ -17,6 +21,16 @@ TOKEN = os.environ.get("SWITCHBOT_TOKEN")
 SECRET = os.environ.get("SWITCHBOT_SECRET")
 GAS_URL = os.environ.get("GAS_URL")
 APP_HOME = os.environ.get("APP_HOME")
+GSPREAD_SHEET_KEY = os.environ.get("GSPREAD_SHEET_KEY")
+GOOGLE_APPLICATION_CREDENTIALS_JSON_FILE_NAME = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON_FILE_NAME")
+
+GOOGLE_CREDENTIAL_FILE_PATH = f"{APP_HOME}/{GOOGLE_APPLICATION_CREDENTIALS_JSON_FILE_NAME}"
+
+DEVICE_ID_ENTRANCE_THERMOMETER = "CA3234352A7A"
+DEVICE_ID_LIVING_THERMOMETER = "D13234353F96"
+
+SHEET_NAME_ENTRANCE = "室温（玄関）"
+SHEET_NAME_LIVING = "室温（リビング）"
 
 # ロギングの設定
 with open(f"{APP_HOME}/log_config.json", "r") as f:
@@ -25,24 +39,32 @@ config.dictConfig(log_conf)
 logger = getLogger(__name__)
 
 
+def __get_meter_info(device_id: str) -> tuple[str, str, str]:
+    header = create_api_header(token=TOKEN, secret=SECRET)
+
+    res = requests.get(
+        f"https://api.switch-bot.com/v1.1/devices/{device_id}/status",
+        headers=header,
+    )
+    info = res.json()
+    now = datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
+    temp = info["body"]["temperature"]
+    hum = info["body"]["humidity"]
+    return (now, temp, hum)
+
+
 def main():
     logger.info("Started")
 
     header = create_api_header(token=TOKEN, secret=SECRET)
     logger.debug(header)
 
-    response = requests.get(
-        "https://api.switch-bot.com/v1.1/devices/CA3234352A7A/status",
-        headers=header,
-    )
-    devices = response.json()
-    logger.debug(devices)
-    temp = devices["body"]["temperature"]
-    hum = devices["body"]["humidity"]
+    entrance_info = __get_meter_info(DEVICE_ID_ENTRANCE_THERMOMETER)
 
-    url = f"{GAS_URL}?t={temp}&h={hum}"
-    response = requests.get(url)
-    logger.debug(response)
+    gspreadsheet = getGoogleSpreadSheet(spread_sheet_id=GSPREAD_SHEET_KEY, credential_file_path=GOOGLE_CREDENTIAL_FILE_PATH)
+    worksheet = gspreadsheet.worksheet(SHEET_NAME_ENTRANCE)
+    worksheet.append_row([entrance_info[0], entrance_info[1], entrance_info[2]])
+    logger.debug(worksheet.get_all_values())
 
     logger.info("Finished")
 
